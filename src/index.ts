01189c527c5431544f520c5f7ba9e18062bae077
ben.hu <ben.hu@wemoscooter.com>
2023-05-14 13:12:39 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,21 @@
-import { add } from "./util/adder";
+import mqtt from "mqtt";
 
-let res = add(2, 3);
-console.log("res", res);
+const local = `mqtt://localhost:1888`;
+const ngrokUrl = `mqtt://0.tcp.jp.ngrok.io:11136`;
+
+const client = mqtt.connect(ngrokUrl);
+
+client.on("connect", function () {
+  client.subscribe("q1", function (err) {
+    if (!err) {
+      client.publish("presence", "Hello mqtt");
+    }
+  });
+});
+
+client.on("message", function (topic, message) {
+  // message is Buffer
+  console.log("topic", topic);
+  console.log(message.toString());
+  client.end();
+});
